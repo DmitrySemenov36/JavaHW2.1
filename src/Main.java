c72f41a6e1b28e011656cbb60a45c36c70725949
Dima <dima98s@mail.ru>
2023-03-05 16:38:28 +0300

    initial commt

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int costInRubles = 2_765;
+        int percentInRubles = 20;
+        int bonus = costInRubles / percentInRubles;
+        System.out.println("Ваш бонус составляет:"+ bonus +" miles");
+    }
+}
